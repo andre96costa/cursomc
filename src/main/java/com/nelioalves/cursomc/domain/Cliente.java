632ca1fe4bfd9f740a0f28c8a73008d631b86e19
Andre Costa <andre2396costa@gmail.com>
2020-12-27 14:51:48 -0300

    Criado Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -34,6 +34,8 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 		
@@ -106,6 +108,16 @@ public class Cliente implements Serializable{
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+	
+	
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
---
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,41 @@
+package com.nelioalves.cursomc.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento{
+	private static final long serialVersionUID = 1L;
+	
+	private Date dataVencimento;
+	private Date dataPagemento;
+	
+	public PagamentoComBoleto() {
+		
+	}
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagemento ) {
+		super(id, estado, pedido);
+		this.dataVencimento = dataVencimento;
+		this.dataPagemento = dataPagemento;
+	}
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	public Date getDataPagemento() {
+		return dataPagemento;
+	}
+	public void setDataPagemento(Date dataPagemento) {
+		this.dataPagemento = dataPagemento;
+	}
+	
+	
+	
+	
+
+}
