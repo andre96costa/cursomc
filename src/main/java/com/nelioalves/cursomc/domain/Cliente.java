e6ae4ba99a61456e7000c0b5a1f18b82a74e9029
Andre Costa <andre2396costa@gmail.com>
2020-12-29 14:41:16 -0300

    PUT, DELETE e GET para CLIENTE

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -47,7 +47,7 @@ public class Cliente implements Serializable{
 		this.nome = nome;
 		this.email = email;
 		this.cpf_ou_cnpj = cpf_ou_cnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo == null) ? null : tipo.getCod();
 	}
 
 	public Integer getId() {
