be4344468ecd27f612e164999448889083d44fc0
Andre Costa <andre2396costa@gmail.com>
2020-12-31 18:12:10 -0300

    Restricao de conteudo: Cliente so recupera ele mesmo

---
+++ src/main/java/com/nelioalves/cursomc/exceptions/AuthorizationException.java
@@ -0,0 +1,20 @@
+package com.nelioalves.cursomc.exceptions;
+
+public class AuthorizationException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+
+	public AuthorizationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public AuthorizationException(String message) {
+		super(message);
+	}
+
+	
+	
+	 
+	
+
+}
--- src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.nelioalves.cursomc.exceptions.AuthorizationException;
 import com.nelioalves.cursomc.exceptions.DataIntregrityException;
 import com.nelioalves.cursomc.exceptions.ObjectNotFoundException;
 
@@ -36,4 +37,10 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	
+	@ExceptionHandler(AuthorizationException.class)
+	public ResponseEntity<StandartError> AuthorizationException(AuthorizationException e, HttpServletRequest request){
+		StandartError err = new StandartError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+	} 
 }
