69a5970478a01b7a801370b8846373fe092e0e1b
Andre Costa <andre2396costa@gmail.com>
2021-01-02 14:37:47 -0300

    Usando padroes de nomes para envido de imagens

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -47,7 +47,7 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy = "cliente")
 	@JsonIgnore
 	private List<Pedido> pedidos = new ArrayList<>();
-	private String imageUrl;
+	
 	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
@@ -151,13 +151,7 @@ public class Cliente implements Serializable{
 		this.perfis.add(perfil.getCod());
 	}
 	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
+	
 	
 
 	@Override
--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.nelioalves.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -41,6 +43,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public List<Cliente> findAll(){
 		return repo.findAll();
@@ -121,13 +129,9 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado!");
 		}
 		
-		URI uri = s3Service.uploadFile(multipartFile);
-		Optional<Cliente> cli = repo.findById(user.getId());
-		if(cli.get() != null) {
-			Cliente cliente = cli.get();
-			cliente.setImageUrl(uri.toString());
-			repo.save(cliente);
-		}
-		return uri;
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 }
