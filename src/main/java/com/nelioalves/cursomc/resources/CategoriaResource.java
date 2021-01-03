779a3fd885a896bc219e3810388b1108633271da
Andre Costa <andre2396costa@gmail.com>
2021-01-03 11:31:47 -0300

    Removido da classe CategoriaResource a restricao que permitia apenas usuarios ADMIN fazer um GET em categorias

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.services.CategoriaService;
 
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
@@ -33,7 +35,7 @@ public class CategoriaResource {
 	private CategoriaService categoriaService;
 	
 	
-	@PreAuthorize("hasAnyRole('ADMIN')")
+	
 	@GetMapping()
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = categoriaService.findAll();
